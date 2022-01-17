7037f047b29693e174041d692e97008d71617f41
Jarvis FW <86500890+JarvisFW@users.noreply.github.com>
2022-01-17 21:44:19 +0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'Jarvis'
+description 'QB LSD'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua'
+}
