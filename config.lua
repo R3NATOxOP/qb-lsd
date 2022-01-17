4c8a26efae5ecc76ef7842ebe6626701e8b48fb3
Jarvis FW <86500890+JarvisFW@users.noreply.github.com>
2022-01-17 21:44:04 +0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = Config or {}
+
+Config.Locale = 'en'
+
+Config.CircleZones = {
+	
+	MushroomField = {coords = vector3(3254.67, -135.77, 15.67), name = 'blip_weedfield', color = 25, sprite = 496, radius = 15.0},
+	
+	MushroomProcessing = {coords = vector3(1391.82, 3605.84, 38.94), name = 'blip_weedprocessing', color = 0, sprite = 6, radius = 10.0},
+}
+
+
+Config.craft1X = 435.8    -- change if you want to move the marker X
+Config.craft1Y = -974.50   -- change if you want to move the marker Y
+Config.craft1Z = 30.00      -- change if you want to move the marker Z
